ad15c322de88a6dd783ce59ad3819a9766298673
Kartikey Choudhary <kartikey31choudhary@gmail.com>
2024-05-17 23:08:29 +0530

    init flowbite fix

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,6 +1,7 @@
 import { CanActivateFn, Router, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import {inject} from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { initFlowbite } from 'flowbite';
 
 export const authGuard: CanActivateFn = (
   route: ActivatedRouteSnapshot, 
@@ -10,7 +11,9 @@ export const authGuard: CanActivateFn = (
     const router:Router = inject(Router)
     const allowedRoutes = ['/login', '/register']
     if(allowedRoutes.includes(state.url)) {return true;}
-    if(authService.isAuthenticated()) {return true;}
+    if(authService.isAuthenticated()) { setTimeout(() => {
+      initFlowbite();
+    }, 200);return true;}
     router.navigate(['/login']);
     return false;
 };
